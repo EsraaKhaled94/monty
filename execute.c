ca481b083bfa2b5a545f8fddf1d6a8f6537912d1
Esraa Khaled <esraakhaled994@gmail.com>
2024-09-26 20:29:45 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -5,7 +5,6 @@
 * @counter: line_counter
 * @file: poiner to monty file
 * @content: line content
-* Return: no return
 */
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
