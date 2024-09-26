40056df14272222a61440673f783ccbad841a882
Esraa Khaled <esraakhaled994@gmail.com>
2024-09-26 20:30:55 +0300

    Update mod.c

--- mod.c
+++ mod.c
@@ -3,8 +3,7 @@
  * f_mod - computes the rest of the division of the second
  * top element of the stack by the top element of the stack
  * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @counter: line number
 */
 void f_mod(stack_t **head, unsigned int counter)
 {
