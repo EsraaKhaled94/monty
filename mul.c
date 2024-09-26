12b128654da0246306196cbe543e0a4cc135c024
Esraa Khaled <esraakhaled994@gmail.com>
2024-09-26 20:31:41 +0300

    Update mul.c

--- mul.c
+++ mul.c
@@ -2,8 +2,7 @@
 /**
  * f_mul - multiplies the top two elements of the stack.
  * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @counter: line number
 */
 void f_mul(stack_t **head, unsigned int counter)
 {
